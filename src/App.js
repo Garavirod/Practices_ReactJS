6831cc4450dba0e09a0311ff78c871f2aced4aac
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-06-25 20:05:45 -0500

    Router react

--- src/App.js
+++ src/App.js
@@ -3,21 +3,48 @@ import Counter from './components/Counter';
 import List from './components/List';
 import Form from './components/Form';
 import FormHook from './components/FormHook';
-import FormOne from './components/FormOne';
+import RouterCompo from './components/RouterCompo';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 function App() {
   return (
    <div>
-       <h1>Hello world!</h1>
-       <Counter/>
+      <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+        <Link className="navbar-brand" to="/">Navbar</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink  activeClassName="active" className="nav-item nav-link" to="/forms">Froms <span className="sr-only">(current)</span></NavLink>
+            <NavLink  activeClassName="active" className="nav-item nav-link" to="/contact">Contact</NavLink>
+            <NavLink  activeClassName="active" className="nav-item nav-link" to="/list">List</NavLink>
+            <NavLink  activeClassName="active" className="nav-item nav-link" to="/counter">Counter</NavLink>          
+          </div>
+        </div>
+      </nav>
+       <h1>Hello world from ReactJS!</h1>
        <hr/>
-       <List/>
-       <hr/>
-       <Form/>
-       <hr/>
-       <FormHook/>
-       <hr/>
-       <FormOne/>
+       {/* Inside 'switch' we're going to load dynamic content */}
+       <Switch>
+         <Route path='/' exact> 
+          <h1>Root content</h1>
+         </Route>                                    
+         <Route path='/contact' component={RouterCompo}/>
+         <Route path='/forms' component={FormHook}/>
+         <Route path='/counter' component={Counter}/>
+         <Route path='/list' component={List}/>
+       </Switch>
+      </Router>
+
+      
        
    </div>
   );
---
+++ src/components/RouterCompo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RouterCompo = ()=>{
+    return(
+        <div className="container">
+            <h1>Dynamic content works</h1>
+            <p>contact</p>
+        </div>
+    )
+}
+
+export default RouterCompo;
